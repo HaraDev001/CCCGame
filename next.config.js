5548dc60361324c12fdacdbb81301c5b7786616d
alan115-skyhand <skyhand500@hotmail.com>
2021-11-25 02:36:32 +0900

    static-public4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
 const withImages = require('next-images')
-module.exports = {
-  reactStrictMode: true,
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: '/public',
-  },
-  ...withImages()
-}
+module.exports = withImages()
